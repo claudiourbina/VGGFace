d384bf9613df87d8a2c0cedd79ac5efdcda3c64b
curbina <claudiourbina.vc@gmail.com>
2022-03-16 16:17:32 -0300

    fix setup config

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import find_packages
 from setuptools import setup
 
-
-REQUIRED_PACKAGES = []
-with open('requirements.txt') as file:
-    REQUIRED_PACKAGES = [line.strip() for line in file]
-
 setup(
-    name="resnet50",
+    name="vggface",
     version="v1.0.0",
-    description="ResNet50 for TFv2",
-    url="https://github.com/claudiourbina/resnet50",
+    description="VGGFace for TFv2",
+    url="https://github.com/claudiourbina/VGGFace",
     author="Claudio Urbina",
     author_email="claudiourbina.vc@gmail.com",
     license="MIT",
     packages=find_packages(exclude=["tests"]),
-    install_requires=[REQUIRED_PACKAGES],
+    install_requires=["pillow==9.0.1"],
     extras_require={
         "cpu": ["tensorflow==2.4.4"],
         "gpu": ["tensorflow-gpu==2.4.4"]
