d4d41c50ae41ef9308be126fc91a98b28888d4a0
curbina <claudiourbina.vc@gmail.com>
2022-03-16 15:52:59 -0300

    add version and requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 
 from vggface.__version__ import __version__
 
+REQUIRED_PACKAGES = []
+with open('requirements.txt') as file:
+    REQUIRED_PACKAGES = [line.strip() for line in file]
+
 setup(
     name="resnet50",
     version=__version__,
@@ -12,7 +16,7 @@ setup(
     author_email="claudiourbina.vc@gmail.com",
     license="MIT",
     packages=find_packages(exclude=["tests"]),
-    install_requires=[],
+    install_requires=[REQUIRED_PACKAGES],
     extras_require={
         "cpu": ["tensorflow==2.4.4"],
         "gpu": ["tensorflow-gpu==2.4.4"]
